50ffe105b879a6f679aa8e45cf398d487a2b4b28
DiegoV25 <121049945+DiegoV25@users.noreply.github.com>
2023-01-15 18:33:03 -0600

    Se agrega l calculadora

---
+++ practica/hola-nombre.js
@@ -0,0 +1,9 @@
+function changeName(params) {
+    let name = prompt("Introduce tu nombre");
+    document.getElementById("Hola").innerHTML="Hola "+name;
+}
+
+function changeName2(params) {
+    let name2 = prompt("Introduce tu nombres");
+    document.getElementById("Hola").innerHTML="Hola "+name2;
+}
